92b914453a65742faec18570d3314c84109604b1
JasiekJasiek <janszala.04@gmail.com>
2025-10-18 18:52:19 +0200

    promt

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from google import genai
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
 client = genai.Client()
 
-response = client.models.generate_content(
-    model="gemini-2.5-pro", contents="Korzystająć z danych GUS podaj mi korzystając z formatu JSON wszystkie dane dot ludności mieszkańców w poszególnych osiedlach w płocku")
+with open('prompt.txt', 'r') as f:
+    response = client.models.generate_content(
+        model="gemini-2.5-pro", contents=f.readlines())
 print(response.text)
